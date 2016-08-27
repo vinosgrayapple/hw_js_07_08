d641bbf925bad0a20709a975ee8feb7125c0bcc6
Serhii Komarychev <komarichev@gmail.com>
2016-08-27 20:23:25 +0300

    tabs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,69 @@
-$( function() {
-    $( "#tabs" ).tabs();
-  } );
+
+$(function() {
+
+	$("ul.tabsСaption li").on("click", function() {
+		if (!$( this ).hasClass( "active" )) {
+
+		$(this).siblings("li").removeClass("active").end().addClass("active");
+
+		var indx = $("ul.tabsСaption li.active").index();
+		$("div.tabs__content").removeClass("active").eq(indx).addClass("active");
+		}
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (function($) {
+
+
+// $(function() {
+
+// 	$('ul.tabsСaption').on('click', 'li:not(.active)', function() {
+// 		$(this).addClass('active').siblings().removeClass('active')
+// 			.closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+// 	});
+
+// });
+
+
+// })(jQuery);
